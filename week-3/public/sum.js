a2198c12110decaa85e4a8fb049c6372f5431a38
Michael Shen <electrycity84@gmail.com>
2022-06-25 02:28:26 +0800

    change to use async/await

--- week-3/public/sum.js
+++ week-3/public/sum.js
@@ -1,28 +1,38 @@
-function example() {
-  fetch(`getData?number=10`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((d) => {
-      document.getElementById("ex").innerHTML = `The factorial of 10 is ${d}`;
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+async function example() {
+  let data = await fetch(`getData?number=10`);
+  let parseData = await data.json();
+  document.getElementById("ex").innerHTML =
+    `The factorial of 10 is ` + parseData;
+
+  //   fetch(`getData?number=10`)
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((d) => {
+  //       document.getElementById("ex").innerHTML = `The factorial of 10 is ${d}`;
+  //     })
+  //     .catch((e) => {
+  //       console.log(e);
+  //     });
 }
 
 async function calculate() {
   const num = document.getElementById("number").value;
-  fetch(`getData?number=${num}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((d) => {
-      document.getElementById(
-        "result"
-      ).innerHTML = `The factorial of ${num} is ${d}`;
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+  let data = await fetch(`getData?number=${num}`);
+  let parseData = await data.json();
+  document.getElementById("result").innerHTML =
+    `The factorial of ${num} is ` + parseData;
+
+  //   fetch(`getData?number=${num}`)
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((d) => {
+  //       document.getElementById(
+  //         "result"
+  //       ).innerHTML = `The factorial of ${num} is ${d}`;
+  //     })
+  //     .catch((e) => {
+  //       console.log(e);
+  //     });
 }
